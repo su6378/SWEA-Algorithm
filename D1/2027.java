e937a7b8ae26b89ce93fdafca56bebada5290b75
su6378 <48742378+su6378@users.noreply.github.com>
2022-05-13 14:48:45 +0900

    SWEA 2027 : 대각선 출력하기
    출력

---
+++ D1/2027.java
@@ -0,0 +1,18 @@
+package Main;
+
+import java.util.*;
+import java.io.*;
+
+class Solution {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		System.out.println("#++++\n"
+				+ "+#+++\n"
+				+ "++#++\n"
+				+ "+++#+\n"
+				+ "++++#");
+	}
+}
+
