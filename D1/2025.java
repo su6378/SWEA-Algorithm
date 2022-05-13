78878f62dbd7768f238d59658ce663f1ab736ef7
su6378 <48742378+su6378@users.noreply.github.com>
2022-05-13 14:52:01 +0900

    SWEA 2025 : N줄덧셈
    반복문

---
+++ D1/2025.java
@@ -0,0 +1,18 @@
+package Main;
+
+import java.util.*;
+import java.io.*;
+
+class Solution {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		int sum = 0;
+		for (int i = 1; i <= num; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
+
