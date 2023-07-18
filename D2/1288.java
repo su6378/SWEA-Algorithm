3aeba30b3484688fc8a51f0a44db2f4c19e7f323
su6378 <josooyeon11@naver.com>
2023-07-18 09:33:39 +0900

    SWEA 1288 : 새로운 불면증 치료법
    D2
    # 구현

--- D2/1288.java
+++ D2/1288.java
@@ -1,40 +1,35 @@
-package Main;
-
 import java.io.*;
-import java.util.*;
 
 class Solution {
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int i = 1; i <= T; i++) {
-			
-			int num = Integer.parseInt(br.readLine());
-			int count = 1;
-			Set<Integer> set = new HashSet<Integer>();
-			while(true) {
-				
-				int num2 = num*count;
-								
-				while(num2 > 0) {
-					set.add(num2 % 10);
-					num2 /= 10;
-				}
-				
-				if(set.size() == 10) break;
-				
-				count++;
-				
-			}
-			
-			bw.write("#"+String.valueOf(i)+" "+String.valueOf(count*num)+"\n");
-		}
-		
-		bw.flush();
-		bw.close();
-	}
-}
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int t = Integer.parseInt(br.readLine());
+
+        for (int tc = 1; tc <= t; tc++) {
+
+            long n = Long.parseLong(br.readLine());
+            int count = 0; // 숫자 카드
+            long k = 0L;
+            boolean[] exist = new boolean[10];
+
+            while (count < 10){
+                k++;
+                long num = n * k;
+                StringBuilder sb = new StringBuilder(String.valueOf(num));
+
+                for (int i = 0; i < sb.length(); i++) {
+                    if (!exist[Character.getNumericValue(sb.charAt(i))]) {
+                        exist[Character.getNumericValue(sb.charAt(i))] = true;
+                        count++;
+                    }
+                }
+            }
+            bw.write("#"+tc+" "+n*k+"\n");
+
+        }
+        bw.flush();
+        bw.close();
+    }
+}
