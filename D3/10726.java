52289503d2ca08e808a2f939a648c1b0f305b39d
su6378 <josooyeon11@naver.com>
2023-07-18 09:55:20 +0900

    SWEA 10726 : 이진수 표현
    D3
    # 문자열

--- D3/10726.java
+++ D3/10726.java
@@ -1,51 +1,28 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 class Solution {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String args[]) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
 
-		int T = Integer.parseInt((br.readLine().trim()));
+        for (int tc = 1; tc <= t; tc++) {
+            StringTokenizer token = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(token.nextToken());
+            int m = Integer.parseInt(token.nextToken());
 
-		for (int tc = 1; tc <= T; tc++) {
-			StringTokenizer token = new StringTokenizer(br.readLine());
+            String binaryM = Integer.toBinaryString(m);
 
-			int N = Integer.parseInt(token.nextToken());
-			int M = Integer.parseInt(token.nextToken());
-
-			String binary = Integer.toBinaryString(M);
-			char[] bit = new char[30];
-			
-			for (int i = 0; i < bit.length; i++) {
-				bit[i] = '0';
-			}
-			
-			int idx = 29;
-			for (int i = binary.length() - 1; i >= 0.; i--) {
-				bit[idx] = binary.charAt(i);
-				idx--;
-				if (idx < 0)
-					break;
-			}
-			boolean check = true;
-			
-			for (int i = 30-N; i < bit.length; i++) {
-				if (bit[i] == '0') {
-					check = false;
-					break;
-				}
-			}
-			
-			bw.write("#"+String.valueOf(tc)+" ");
-			
-			if (check) {
-				bw.write("ON\n");
-			}else bw.write("OFF\n");
-		}
-		bw.flush();
-		bw.close();
-	}
+            if (binaryM.length() < n) bw.write("#"+tc+" "+"OFF\n"); // 마지막 N 비트보다 이진수의 길이가 적으면 OFF
+            else{
+                String num = binaryM.substring(binaryM.length() - n);
+                if (Integer.parseInt(num,2) == (int)Math.pow(2.0,n)-1) bw.write("#"+tc+" "+"ON\n");
+                else bw.write("#"+tc+" "+"OFF\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
 }
-
