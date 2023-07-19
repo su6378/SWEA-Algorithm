00dc1d4cddc4c31e4777c9b0a8fbcb646fa05735
su6378 <josooyeon11@naver.com>
2023-07-19 09:58:37 +0900

    SWEA 1230 : 암호문 3
    D3
    # 리스트

--- D3/1230.java
+++ D3/1230.java
@@ -1,51 +1,77 @@
-package Main;
-
 import java.io.*;
 import java.util.*;
 
 class Solution {
-	public static void main(String args[]) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		StringBuffer sb = new StringBuffer();
-		
-		for (int i = 1; i <= 10; i++) {
-			sb.append("#").append(i).append(" ");
-			int N = sc.nextInt();
-			List<Integer> passwords = new LinkedList<>();
-            for(int j = 0; j<N; j++){
-            	passwords.add(sc.nextInt());
-            }
-			
-			int commands = sc.nextInt();
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-			for (int j = 0; j < commands; j++) {
-				char c = sc.next().charAt(0);
-				if (c == 'I') {
-					int X = sc.nextInt();
-					int Y = sc.nextInt();
-					int cnt = X + Y;
-					for (int k = X; k < cnt; k++) {
-						passwords.add(k, sc.nextInt());
-					}
-				} else if (c == 'D') {
-					int X = sc.nextInt();
-					int Y = sc.nextInt();
-					int cnt = X + Y;
-					for (int k = X; k < cnt; k++) {
-						passwords.remove(X);
-					}
-				} else {
-					int Y = sc.nextInt();
-					for (int k = 0; k < Y; k++) {
-						passwords.add(sc.nextInt());
-					}
-				}
-			}
-			for(int j=0; j<10; j++){
-                sb.append(passwords.get(j)).append(" ");
+        for (int tc = 1; tc <= 10; tc++) {
+            int n = Integer.parseInt(br.readLine());
+
+            List<String> list = new LinkedList<>();
+
+            StringTokenizer token = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < n; i++) {
+                list.add(token.nextToken());
             }
-            sb.append("\n");
-		}
-		System.out.println(sb);
-	}
+
+            int m = Integer.parseInt(br.readLine());
+
+            token = new StringTokenizer(br.readLine());
+
+            for (int i = 0; i < m; i++) {
+                String state = token.nextToken();
+
+                switch (state) {
+                    case "I":
+                        int x = Integer.parseInt(token.nextToken());
+                        int y = Integer.parseInt(token.nextToken());
+
+                        List<String> temp = new LinkedList<>();
+
+                        for (int j = 0; j < y; j++) {
+                            temp.add(token.nextToken());
+                        }
+
+                        list.addAll(x, temp);
+                        break;
+
+                    case "D":
+                        x = Integer.parseInt(token.nextToken());
+                        y = Integer.parseInt(token.nextToken());
+
+                        for (int j = 0; j < y; j++) {
+                            list.remove(x);
+                        }
+                        break;
+
+                    case "A":
+                        y = Integer.parseInt(token.nextToken());
+
+                        temp = new LinkedList<>();
+
+                        for (int j = 0; j < y; j++) {
+                            String s = token.nextToken();
+                            temp.add(s);
+                        }
+
+                        list.addAll(temp);
+                        break;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+            bw.write("#"+tc+" ");
+
+            for (int i = 0; i < 10; i++) {
+                sb.append(list.get(i)+" ");
+            }
+
+            bw.write(sb+"\n");
+        }
+        bw.flush();
+        bw.close();
+    }
 }
